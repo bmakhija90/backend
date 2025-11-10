52b46975942c7bf6a17417229808ab0e4d914065
Bunty Makhija <bunty.mystic@gmail.com>
2025-11-11 02:34:39 +0530

    Allowed all origins

--- Program.cs
+++ Program.cs
@@ -13,21 +13,12 @@ builder.Services.AddSingleton<JwtService>();
 // Add CORS services
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngularApp", policy =>
-    {
-        policy.WithOrigins("http://localhost:4200") // Angular dev server
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials(); // Required for authentication cookies/tokens
-    });
-
     // For production, add your production Angular app URL
     options.AddPolicy("AllowProduction", policy =>
     {
-        policy.WithOrigins("https://your-angular-app.com")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        policy.AllowAnyOrigin()
+             .AllowAnyHeader()
+             .AllowAnyMethod();
     });
 });
 
@@ -58,7 +49,7 @@ builder.Services.AddControllers();
 
 
 var app = builder.Build();
-app.UseCors("AllowAngularApp");
+app.UseCors("AllowProduction");
 
 app.UseAuthentication();
 app.UseAuthorization();
